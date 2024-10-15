e9fc82503ee2a12ef8f46b2c4833b28cabe99ccd
Gilad <gilad.smith@yahoo.com>
2024-10-15 14:21:57 +0100

    initial commit

--- consolidation_exercises/Week_05_robot/robot_challenge.py
+++ consolidation_exercises/Week_05_robot/robot_challenge.py
@@ -1,4 +1,5 @@
 from cpa_robot import position_x, position_y, orientation, drive, plot_path, reset_robot, plot_robot, sensor_left, sensor_middle, sensor_right
+import random
 reset_robot()
 
 # edit this comment - challenge is to ???
