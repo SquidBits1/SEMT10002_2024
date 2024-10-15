75a4524049e0f2548d122765c87af33ebdce85b0
Gilad <gilad.smith@yahoo.com>
2024-10-15 14:41:41 +0100

    Added description

--- consolidation_exercises/Week_05_robot/robot_challenge.py
+++ consolidation_exercises/Week_05_robot/robot_challenge.py
@@ -2,7 +2,7 @@ from cpa_robot import position_x, position_y, orientation, drive, plot_path, res
 import random
 reset_robot()
 
-# edit this comment - challenge is to ???
+# edit this comment - challenge is to have the robot move like a roomba. Any time it hits the circle it should turn by a random angle and carry on moving. Any time it hits a wall it should turn 180 degrees.???
 '???'
 
 plot_path()
